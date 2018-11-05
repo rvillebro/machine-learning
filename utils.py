f310acdc1c2bdc9cb3606eccb0d49df0de556953
Rasmus Villebro <rasmus-villebro@hotmail.com>
2018-11-05 15:00:12 +0100

    added utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import numpy as np
+from numpy.random import permutation
+
+class DataSet():
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def normalize(self):
+        pass
+
+    def shuffle(self):
+        
